db31b4230b1a13bd608b10642548d227f98861f6
Sifiso Mazibuko <sifisomazibuko@hotmail.com>
2017-06-08 08:24:13 +0200

    Added a Rest WebService to perform the GET-POST-DELETE

---
+++ DSApp/DSApp/Services/CustomerService.cs
@@ -0,0 +1,20 @@
+﻿using DSApp.Models;
+using Plugin.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSApp.Services
+{
+    public class CustomerService
+    {
+        public async Task<List<Customers>> PostCustomer()
+        {
+            RestClient<Customers> restClient = new RestClient<Customers>();
+            var customer = await restClient.GetAsync();
+            return customer;
+        }
+    }
+}
