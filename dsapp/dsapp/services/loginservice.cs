c36ac203329ae9149ef11db62c53822ef09a36fb
Sifiso Mazibuko <sifisomazibuko@hotmail.com>
2017-06-12 07:32:29 +0200

    Updates

--- dsapp/dsapp/services/loginservice.cs
+++ dsapp/dsapp/services/loginservice.cs
@@ -7,11 +7,13 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using DSApp.Models;
+using System.Diagnostics;
 
 namespace DSApp.Services
 {
     public  class LoginService
     {
+        private const string WebServiceUrl = "http://localhost:30421/api/Account/";
         public async Task<bool> LoginAsync(string userName, string password)
         {
             var keyValues = new List<KeyValuePair<string, string>>
@@ -19,6 +21,12 @@ namespace DSApp.Services
                 new KeyValuePair<string, string>("username", userName),
                 new KeyValuePair<string, string>("password", password),
             };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, WebServiceUrl);
+            request.Content = new FormUrlEncodedContent(keyValues);
+            var client = new HttpClient();
+            var response = await client.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
             return true;
         }
     }
